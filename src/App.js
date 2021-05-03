2dc4efee82eee4dcf7df5b4c82d30dfc3769e11c
brunadossantos-tech <74012238+brunadossantos-tech@users.noreply.github.com>
2021-05-03 10:25:37 +0200

    added GitHub url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      weather app
+      <h1>
+        Weather App
+      </h1>
+      <footer>
+        This project was coded by {""}
+        <a href="https://practical-goldberg-8caec7.netlify.app/" target="_blank">Bruna Santos</a> and is{" "}
+        <a href="https://github.com/brunadossantos-tech/react-weather-search.git" target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
