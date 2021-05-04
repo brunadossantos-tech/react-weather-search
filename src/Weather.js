b191df783026b9ad579c6fdf53bea0d5a3c4e6c9
brunadossantos-tech <74012238+brunadossantos-tech@users.noreply.github.com>
2021-05-04 11:56:58 +0200

    import weather icons

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props){
             wind: response.data.wind.speed,
             humidity: response.data.main.humidity,
             description: response.data.weather[0].description,
-            iconUrl:"https://ssl.gstatic.com/onebox/weather/64/rain.png",
+            icon: response.data.weather[0].icon,
             date: new Date(response.data.dt * 1000)
         });
     }
