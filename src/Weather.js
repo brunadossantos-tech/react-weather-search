a8df998bce96c92257ebe418020aa094ab1681c0
brunadossantos-tech <74012238+brunadossantos-tech@users.noreply.github.com>
2021-05-04 11:00:22 +0200

    add currentDate component

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import CurrentDate from "./CurrentDate";
 import axios from "axios";
 import "./Weather.css";
 
@@ -15,7 +16,7 @@ export default function Weather(props){
             humidity: response.data.main.humidity,
             description: response.data.weather[0].description,
             iconUrl:"https://ssl.gstatic.com/onebox/weather/64/rain.png",
-            date: "Monday, 11:00"
+            date: new Date(response.data.dt * 1000)
         });
     }
 
@@ -43,7 +44,7 @@ export default function Weather(props){
             {weatherData.city}
         </h1>
         <ul>
-            <li>{weatherData.date}</li>
+            <li><CurrentDate date={weatherData.date}/></li>
             <li className="text-capitalize"> {weatherData.description}</li>
         </ul>
         <div className="row mt-3">
