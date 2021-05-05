d61f7c1d9092a81186a2023c649e332a3c4b1f9a
brunadossantos-tech <74012238+brunadossantos-tech@users.noreply.github.com>
2021-05-05 11:43:03 +0200

    add ForecastDaily component

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -25,7 +25,7 @@ export default function WeatherIcon(props){
     return(
         <ReactAnimatedWeather
             icon={codeMapping[props.code]}
-            color="#ffcab0"
+            color={props.color}
             size={props.size}
             animate={true}
             />
